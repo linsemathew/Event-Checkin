316875fd7e408a4b1b0bf0435b53dc070d3c2d92
Linse Mathew <linsemathew123@gmail.com>
2016-09-23 15:38:09 -0400

    Add register and login routes.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-var checkin = angular.module('eventCheckin', []);
+var checkin = angular.module('eventCheckin', ['ngRoute']);
+
+checkin.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/login', {
+		templateUrl: 'views/login.html'
+		controller: 'RegistratonController'
+	}).
+	when('/register', {
+		templateUrl: 'views/register.html',
+		controller: 'RegistratonController'
+	}).
+	when('/success', {
+		templateUrl: 'views/success.html',
+		controller: 'SuccessController'
+	}).
+	otherwise({
+		redirectTo: '/login'
+	});
+}]);
 
 checkin.controller('checkinController', ['$scope', function($scope){
-		$scope.message = "Welcome"
+		$scope.message = "Welcome";
 }])
